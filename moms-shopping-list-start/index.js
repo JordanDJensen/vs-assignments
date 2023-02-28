4ef87cb2b77f02fe9127e023b035657bf905b7f6
Jordan Jensen <jojo.jensen03@gmail.com>
2023-02-28 10:56:16 -0700

    created shopping list where you can add and remove an item.

--- moms-shopping-list-start/index.js
+++ moms-shopping-list-start/index.js
@@ -6,15 +6,37 @@ form.addEventListener("submit", function(event){
     form.title.value = ""
 
     let li = document.createElement('li');
+   
+
     let div = document.createElement("div");
+    let btnEdit = document.createElement("button");
+    let btnRemove = document.createElement("button");
+
+    li.setAttribute("name", "shopping")
+    btnRemove.classList.add("removeItem");
     div.textContent = newItem;
+    
+    btnEdit.textContent = "edit";
+    btnRemove.textContent = "X";
 
     li.appendChild(div);
+    li.appendChild(btnRemove);
+    li.appendChild(btnEdit);
+
+   
+    
 
     let listItem = document.getElementById("list");
     listItem.appendChild(li);
 
+    btnRemove.addEventListener("click", () =>{
+        var removeItem = document.getElementsByClassName("removeItem");
+        for(let i =0; i < removeItem.length; i++){
+           li.style.display = "none";
+        }
+    });
 
-
+    
 });
+
 
