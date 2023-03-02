252c6d00373cd97d6ed810eb223bcc22b225c0d8
Jordan Jensen <jojo.jensen03@gmail.com>
2023-03-02 13:15:54 -0700

    calculator works now where you can add, subtract, divide, multiple

--- exercises/node-calculator/index.js
+++ exercises/node-calculator/index.js
@@ -1,21 +1,40 @@
 const readline = require("readline-sync");
-var val = readline.question("please enter your first number ");
-var val2 = readline.question("please enter your second number ");
+var val = readline.questionInt("please enter your first number ");
+var val2 = readline.questionInt("please enter your second number ");
 
-function add(){
-    result = val + val2
-    console.log("The results is: " + result);
-}
+
 var op = readline.question("what operation do you want to perform?" +  "\n(a)add \n(s)subtract \n(d)divide \n(m)multiply\n");
 
-if(op== "a"){
-    add(val, val2)
+function add(num1, num2){
+    results = num1 + num2;
+    return results
+}
+function subtract(num1, num2){
+    results = num1 - num2;
+    return results;
+}
+function divide(num1, num2){
+    results = num1 / num2;
+    return results; 
+}
+function multiply(num1, num2){
+    results = num1 * num2; 
+    return results;
+}
+
+
+if(op == "a"){
+    
+    console.log ("The result is : " + add(val, val2));
 } else if(op == "s"){
-    console.log("subtract");
+    console.log("The results is: " + subtract(val, val2));
+    
 } else if(op == "m"){
-    console.log("multiply");
+    console.log("The results is: " + multiply(val, val2));
+
 }else if(op == "d"){
-    console.log("divide");
+    console.log("The results is: " + divide(val, val2));
+
 } else {
     return;
 }
