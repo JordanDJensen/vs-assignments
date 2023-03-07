a393d800b8e7818bdd8c73391aa996bc12a22189
Jordan Jensen <jojo.jensen03@gmail.com>
2023-03-07 11:40:56 -0700

    Finished loop eexercise until bronze

---
+++ exercises/loop-olympics/index.js
@@ -0,0 +1,24 @@
+// 1 
+for(let i = 0; i < 10; i++){
+    console.log(i + "\n");
+}
+
+//2  
+for(let i = 9; i > -1; i--){
+    console.log(i);
+}
+//3
+const fruit = ["banana", "orange", "apple", "kiwi", "pear", "peach"]
+let newFruit = [];
+for(let i = 0; i < fruit.length; i+= 2){
+ 
+        
+        newFruit.push(fruit[i]);
+    
+}
+console.log(newFruit);
+
+// Bronze 
+//1
+//2
+//3 
